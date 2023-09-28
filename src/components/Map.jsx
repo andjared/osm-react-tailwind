ef081d1b935a07d96869435d566a52f52ae59a42
andjared <andja.velimirovic@gmail.com>
2023-09-28 11:06:38 +0200

    Add Mapbox custom map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,11 +1,20 @@
 import "leaflet/dist/leaflet.css";
+
 import { MapContainer, TileLayer } from "react-leaflet";
 
 export default function Map() {
   return (
-    <MapContainer center={[44.12002775845974, 15.232276147257753]} zoom={8} className="h-5/6 w-3/4 ">
-        <TileLayer attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"></TileLayer>
+    <MapContainer
+      center={[44.12002775845974, 15.232276147257753]}
+      zoom={8}
+      className="h-5/6 w-3/4 "
+    >
+      <TileLayer
+        attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        url={`https://api.mapbox.com/styles/v1/andja-z/clmz7sti002zy01r7eih71srx/tiles/256/{z}/{x}/{y}?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+      />
     </MapContainer>
-  )
+  );
 }
+
+// mapbox://styles/andja-z/clmz7sti002zy01r7eih71srx
