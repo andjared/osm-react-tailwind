4e3970cfb5a8729c1138121b92ba9c47ccbdb3bc
andjared <andja.velimirovic@gmail.com>
2023-09-28 11:12:19 +0200

    Set map center

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,14 +1,12 @@
 import "leaflet/dist/leaflet.css";
+import { startTransition, useState } from "react";
 
 import { MapContainer, TileLayer } from "react-leaflet";
 
 export default function Map() {
+  const [center, setCenter] = useState([44.8, 15.8]);
   return (
-    <MapContainer
-      center={[44.12002775845974, 15.232276147257753]}
-      zoom={8}
-      className="h-5/6 w-3/4 "
-    >
+    <MapContainer center={center} zoom={7.5} className="h-5/6 w-3/4 ">
       <TileLayer
         attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         url={`https://api.mapbox.com/styles/v1/andja-z/clmz7sti002zy01r7eih71srx/tiles/256/{z}/{x}/{y}?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
