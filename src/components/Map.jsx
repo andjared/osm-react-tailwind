6474d48e52e15f6f6cda8c80347161f7a6c29b32
andjared <andja.velimirovic@gmail.com>
2023-09-29 09:27:02 +0200

    Add map markers

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,6 +1,6 @@
 import "leaflet/dist/leaflet.css";
 import { useEffect, useState } from "react";
-import { MapContainer, Marker, TileLayer } from "react-leaflet";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
 export default function Map() {
   const [center, setCenter] = useState([44.8, 15.8]);
@@ -17,12 +17,18 @@ export default function Map() {
     getData();
   }, []);
 
+  console.log("hello");
   return (
     <MapContainer center={center} zoom={7.5} className="h-5/6 w-3/4 ">
       <TileLayer
         attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         url={`https://api.mapbox.com/styles/v1/andja-z/clmz7sti002zy01r7eih71srx/tiles/256/{z}/{x}/{y}?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
       />
+      {data.map((property) => {
+        const { geocode, id, popup } = property;
+        console.log(id);
+        return <Marker position={geocode} key={id}></Marker>;
+      })}
     </MapContainer>
   );
 }
