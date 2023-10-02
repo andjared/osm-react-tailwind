fd17b4a186ad74f1bb2e99835cfc608ad74083c5
andjared <andja.velimirovic@gmail.com>
2023-10-02 09:19:36 +0200

    Add headers to fetch

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -2,24 +2,26 @@ import "leaflet/dist/leaflet.css";
 import { useEffect, useState } from "react";
 import { MapContainer, TileLayer } from "react-leaflet";
 import CustomMarker from "./CustomMarker";
-import properties from "../properties.json";
 
 export default function Map() {
   const [center, setCenter] = useState([44.359, 15.876]);
-  // const [data, setData] = useState([]);
+  const [data, setData] = useState([]);
 
-  // const getData = async () => {
-  //   const response = await fetch("../properties.json");
-  //   console.log(response);
-  //   const data = await response.json();
-  //   console.log(data);
-  //   setData(data);
-  //   return data;
-  // };
+  const getData = async () => {
+    const response = await fetch("properties.json", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    });
+    const data = await response.json();
+    setData(data);
+    return data;
+  };
 
-  // useEffect(() => {
-  //   getData();
-  // }, []);
+  useEffect(() => {
+    getData();
+  }, []);
 
   return (
     <MapContainer center={center} zoom={7.5} className="h-5/6 w-2/4">
@@ -27,17 +29,18 @@ export default function Map() {
         attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         url={`https://api.mapbox.com/styles/v1/andja-z/clmz7sti002zy01r7eih71srx/tiles/256/{z}/{x}/{y}?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
       />
-      {properties.map((property) => {
-        const { geocode, id, popup, price } = property;
-        return (
-          <CustomMarker
-            geocode={geocode}
-            key={id}
-            popupText={popup}
-            price={price}
-          />
-        );
-      })}
+      {data.length > 0 &&
+        data.map((property) => {
+          const { geocode, id, popup, price } = property;
+          return (
+            <CustomMarker
+              geocode={geocode}
+              key={id}
+              popupText={popup}
+              price={price}
+            />
+          );
+        })}
     </MapContainer>
   );
 }
